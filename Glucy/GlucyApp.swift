7dd4f3fcdada443b0d75118953b2b741621e56fc
Jesus Santiago <sanjesvel@outlook.com>
2026-09-19 13:21:52 -0600

    feat: proyecto de Xcode con Swift 6, concurrencia estricta e iOS 17 mínimo

---
+++ Glucy/GlucyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlucyApp.swift
+//  Glucy
+//
+//  Created by Jesus Santiago Velasco on 19/09/26.
+//
+
+import SwiftUI
+
+@main
+struct GlucyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
